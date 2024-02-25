35662ae6798f2b787b81c9a90dba68c785b65817
Venkatinadipinti <154719483+Venkatinadipinti@users.noreply.github.com>
2024-02-25 20:20:10 +0530

    Add files via upload

---
+++ src/Mainarea.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Mainarea.css'
+const Mainarea = () => {
+  return (
+    <div className='Main'>
+      <div className='Rside'>
+        <div className='profile'>
+         <img src="https://mastimorning.com/wp-content/uploads/2023/07/cool-whatsapp-dp-Walllpaper-Free.jpg " alt="dp"style={{height:'60px',padding:'1rem'}}/>
+            venkati nadipinti
+        </div>
+        <div className='pro'>
+        <i class="fa fa-user" aria-hidden="true"color='blue'fontSize='2rem'></i>
+        <div>Friends</div>
+        </div>
+        <div className='pro'>
+        <i class="fa fa-user" aria-hidden="true"color='green'fontSize='2rem'></i>
+        <div>Friends</div>
+        </div>
+        <div className='pro'>
+        <i class="fa fa-user" aria-hidden="true"color='blue'fontSize='2rem'></i>
+        <div>Friends</div>
+        </div>
+        <div className='pro'>
+        <i class="fa fa-user" aria-hidden="true"color='blue'fontSize='2rem'></i>
+        <div>Friends</div>
+        </div>
+        <div className='pro'>
+        <i class="fa fa-user" aria-hidden="true"color='blue'fontSize='2rem'></i>
+        <div>Friends</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Mainarea
